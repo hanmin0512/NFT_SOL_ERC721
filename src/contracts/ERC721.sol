12f7ffd9471849f2ff267ce8a572414fd13ea1bd
hanmin0512 <hanmin9981@naver.com>
2024-09-08 14:39:01 +0000

    web3.0 second

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -14,26 +14,50 @@ pragma solidity ^0.8.0;
 contract ERC721 {
 
     event Transfer(
-    address indexed from, 
-    address indexed to, 
-    uint256 indexed tokenId);
+        address indexed from, 
+        address indexed to, 
+        uint256 indexed tokenId);
+    
+    event Approval(
+        address indexed owner,
+        address indexed approved,
+        uint256 indexed tokenId);
+    
+    
 
     //매핑은 해시테이블로 파이썬의 딕셔너리랑 똑같다.
     mapping(uint256 => address) private _tokenOwner;
     
     //소유자의 소유 갯수 토큰 매핑해보십시오
     mapping(address => uint256) private _ownedTokensCount;
+    
+    // 토큰Id와 address 매핑하여 승인하기위한 변수
+    mapping(uint256 => address) private _tokenApprovals;
 
     
+    function balanceOf(address _owner) public view returns(uint256){
+        require(_owner != address(0), 'owner query for non-existent token');
+        return _ownedTokensCount[_owner];
+        
+    }
+    
+    
+    function ownerOf(uint256 _tokenId) public view returns(address){
+        require(_tokenOwner[_tokenId] != address(0));
+        return _tokenOwner[_tokenId];
+    }
+    
 
-    function _exists(uint256 tokenId) internal view returns(bool) {
+    function _exists(uint256 tokenId) public view returns(bool) {
         address owner = _tokenOwner[tokenId];
         // 소유자 주소가 address(0) (즉, 초기값, 설정되지 않음)인인지 아닌지 확인
         return owner != address(0);
     }
 
-
-    function _mint(address to, uint256 tokenId) internal{
+    
+    //virtual 키워드는 자식 클래스에서 상속받을 수 있도록 표시하는것 자식 클래스에서 override 라고 표시가되면 같은이름의 함수를 오버라이딩 할 수 있게 됨.
+    // 
+    function _mint(address to, uint256 tokenId) public virtual{
         //require문으로 주소 설정이 되어있는지 안되어 있는지 확인하고
         require(to != address(0),'ERC721: minting to the zero address '); //주소가 0이면 안된다.
         
@@ -48,4 +72,37 @@ contract ERC721 {
         // emit은 event 함수를 실행시키는 예약어 이다.
         emit Transfer(address(0), to, tokenId);
     }
+    
+    
+    function _transferFrom(address _from, address _to, uint256 _tokenId) internal{
+        
+        require(_to != address(0), 'Error - ERC721 Transfer to the zero address');
+        require(ownerOf(_tokenId) == _from, 'Trying to transfer a token the address does not exists');
+        _ownedTokensCount[_from] -= 1;
+        _ownedTokensCount[_to] += 1;
+        _tokenOwner[_tokenId] = _to;
+        
+        emit Transfer(_from, _to, _tokenId);
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _tokenId) public{
+        require(isApprovedOrOwner(msg.sender, _tokenId), '' );
+        _transferFrom(_from, _to, _tokenId);
+    }
+    
+    function approve(address _to, uint256 tokenId) public {
+        address owner = ownerOf(tokenId);
+        require(_to != owner, 'Error - approval to current owner');
+        require(msg.sender == owner, 'Current caller is not the owner of the token');
+        _tokenApprovals[tokenId] = _to;
+        
+        emit Approval(owner, _to, tokenId);
+    }
+    
+    function isApprovedOrOwner(address spender, uint256 tokenId) internal view returns(bool) {
+        require(_exists(tokenId), 'token does not exists');
+        address owner = ownerOf(tokenId); 
+        return (spender == owner);
+    }
+    
 }
